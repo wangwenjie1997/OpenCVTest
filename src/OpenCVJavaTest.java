33afcea0db2a7e9986b59dc061264fe76318c8c5
wwj <2589856016@qq.com>
2019-03-24 16:14:33 +0800

    OpenCV图片读入，显示，输出

--- src/OpenCVJavaTest.java
+++ src/OpenCVJavaTest.java
@@ -1,20 +1,34 @@
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+import static org.opencv.highgui.HighGui.*;
 
 public class OpenCVJavaTest{
 
+    // 载入dll（必须先加载）
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to OpenCV " + Core.VERSION);
-        Mat m = new Mat(5, 10, CvType.CV_8UC1, new Scalar(0));
-        System.out.println("OpenCV Mat: " + m);
-        Mat mr1 = m.row(1);
-        mr1.setTo(new Scalar(1));
-        Mat mc5 = m.col(5);
-        mc5.setTo(new Scalar(5));
-        System.out.println("OpenCV Mat data:\n" + m.dump());
+
+        //读入图片
+        Mat srcImage = Imgcodecs.imread("C:\\Users\\Administrator\\Desktop\\photo\\opencv1.jpg");
+
+        if(srcImage.dataAddr()==0)
+            System.out.println("图片读入出错");
+        else{
+            namedWindow("显示图片",WINDOW_NORMAL);
+            imshow("显示图片",srcImage);
+            waitKey(0);
+            if(Imgcodecs.imwrite("C:\\Users\\Administrator\\Desktop\\OCRResult\\test.bmp", srcImage))
+                System.out.println("图片保存成功");
+            else
+                System.out.println("图片保存失败");
+        }
+
+        srcImage.release();
+
+//        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+//        System.out.println("mat = " + mat.dump());
     }
 }
